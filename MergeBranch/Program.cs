79f2076d54194a7bbc00d04a54f974fa4344bf04
Daniela Escobar <daniela.b.escobar@gmail.com>
2021-11-18 12:00:46 +0100

    Add project files.

---
+++ MergeBranch/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeBranch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Do not overwrite this");
+            Console.WriteLine("Overwrite this");
+        }
+    }
+}
